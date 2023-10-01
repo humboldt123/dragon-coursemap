e76796c688d28377ac0bbb224126af39ea5d37b0
vish <38266782+humboldt123@users.noreply.github.com>
2023-09-30 22:38:40 -0400

    refactor: comment + variable names

--- src/course.rs
+++ src/course.rs
@@ -16,6 +16,7 @@ impl Course {
         let raw_prerequisite_data = Self::pull_coursedata(&coursedata, r#"<b>Prerequisites:</b> (.*?)\n<br/><br/></div>"#);
         
         // todo: error handling
+        // todo: some courses do not have prereqs. so, again, error handling
         Course {
             code: code.to_string(),
             name: Self::pull_coursedata(&coursedata, r#"</span><span class='cdspacing'>(.*?)</span>"#),
@@ -40,18 +41,20 @@ impl Course {
     }
 
     fn parse_prerequisites(raw_prerequisites: &str) -> Vec<Vec<String>> {
+        // Find and remove the contents of square bracket pairs (including the brackets) plus the whitespace preceeding it
+        // eg; "COURSE 101 [Min Grade: C] and COURSE 102 [Min Grade: B]" becomes "COURSE 101 and COURSE 102"
         let pattern = Regex::new(r#"\s\[.*?\]"#).unwrap();
         let cleaned = pattern.replace_all(raw_prerequisites, "");
     
         let mut prerequisites: Vec<Vec<String>> = vec![];
         for item in cleaned.split(" and ").collect::<Vec<&str>>() {
-            // If it contains a bracket, the student pick the prerequisite from the list. ie; (MATH 221 or MATH 222)
             if item.contains('(') {
-                let mut picky_prerequisite: Vec<String> = vec![];
+                // If it contains a bracket, the student just has to fufill one of the prerequisites from the list. ie; (MATH 221 or MATH 222)
+                let mut selectable_prerequisite: Vec<String> = vec![];
                 for course in item[1..item.len() - 1].split(" or ").collect::<Vec<&str>>() {
-                    picky_prerequisite.push(course.to_string());
+                    selectable_prerequisite.push(course.to_string());
                 }
-                prerequisites.push(picky_prerequisite)
+                prerequisites.push(selectable_prerequisite)
             } else {
                 prerequisites.push(vec![item.to_string()]);
             }

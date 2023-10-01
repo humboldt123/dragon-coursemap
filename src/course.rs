6f70b2e45e50b35ce6c3b78ad88662a5f0cc7478
vish <38266782+humboldt123@users.noreply.github.com>
2023-10-01 13:08:50 -0400

    feat: add courses to map

--- src/course.rs
+++ src/course.rs
@@ -3,10 +3,10 @@ use regex::Regex;
 
 #[derive(Debug)]
 pub struct Course {
-    code: String,
-    name: String,
-    description: String,
-    prerequisites: Vec<Vec<String>>
+    pub code: String,
+    pub name: String,
+    pub description: String,
+    pub prerequisites: Vec<Vec<String>>
 }
 
 
@@ -14,20 +14,32 @@ impl Course {
     pub async fn new(code: &str) -> Course {
         let coursedata = Self::get_raw_coursedata(code).await.unwrap();
         let mut prerequisites: Vec<Vec<String>> = vec![];
+
+        match Self::pull_coursedata(&coursedata, r#"</span><span class='cdspacing'>(.*?)</span>"#) {
+            Ok(value) => {
+                match Self::pull_coursedata(&coursedata, r#"<b>Prerequisites:</b> (.*?)\n<br/><br/></div>"#) {
+                    Ok(value) => prerequisites = Self::parse_prerequisites(&value),
+                    _ => (), // if not leave prerequisites empty
+                }
         
-        // if this course has prerequisites, add them to our array
-        match Self::pull_coursedata(&coursedata, r#"<b>Prerequisites:</b> (.*?)\n<br/><br/></div>"#) {
-            Ok(value) => prerequisites = Self::parse_prerequisites(&value),
-            _ => (), // if not leave prerequisites empty
+                Course {
+                    code: code.to_string(),
+                    name: value,
+                    description: Self::pull_coursedata(&coursedata, r#"<p class="courseblockdesc">\n(.*?)<br />\n"#).unwrap_or("No course description provided.".to_string()),
+                    prerequisites: prerequisites
+                }
+            },
+            Err(error) => {
+                Course {
+                    code: code.to_string(),
+                    name: code.to_string(),
+                    description: error,
+                    prerequisites: prerequisites,
+                }
+            }
         }
 
-        // todo: if course is empty return an empty course struct (or panic?)
-        Course {
-            code: code.to_string(),
-            name: Self::pull_coursedata(&coursedata, r#"</span><span class='cdspacing'>(.*?)</span>"#).unwrap(),
-            description: Self::pull_coursedata(&coursedata, r#"<p class="courseblockdesc">\n(.*?)<br />\n"#).unwrap(),
-            prerequisites: prerequisites
-        }
+        // If this course has prerequisites, add them to our arr
     }
 
     async fn get_raw_coursedata(course: &str) -> Result<String, reqwest::Error> {
@@ -41,7 +53,7 @@ impl Course {
             let content = captures.get(1).unwrap().as_str();
             Ok(content.to_string())
         } else {
-            Err(format!("Failed to parse course data with pattern: {}", regex))
+            Err(format!("Failed to parse course data.\nPattern: {}", regex))
         }
     }
 
@@ -53,14 +65,20 @@ impl Course {
     
         let mut prerequisites: Vec<Vec<String>> = vec![];
         for item in cleaned.split(" and ").collect::<Vec<&str>>() {
-            if item.contains('(') {
-                // If it contains a bracket, the student just has to fufill one of the prerequisites from the list. ie; (MATH 221 or MATH 222)
+            if item.contains(" or ") {
+                // If there are multiple courses, the student just has to fufill one of the prerequisites from the list.
+                // ie; (MATH 221 or MATH 222)
                 let mut selectable_prerequisites: Vec<String> = vec![];
-                // get rid of the brackets with substring
-                for course in item[1..item.len() - 1].split(" or ").collect::<Vec<&str>>() {
+
+                // get rid of the brackets with substring if they exist
+                let cleaned = if item.contains('(') {&item[1..item.len() - 1]} else {item};
+                
+                for course in cleaned.split(" or ").collect::<Vec<&str>>() {
                     selectable_prerequisites.push(course.to_string());
                 }
                 prerequisites.push(selectable_prerequisites)
+                
+
             } else {
                 prerequisites.push(vec![item.to_string()]);
             }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
 mod course;
 
+
+use futures::future::{BoxFuture, FutureExt};
 use std::collections::HashMap;
 use crate::course::Course;
 
 #[tokio::main]
 async fn main() {
     let mut courses: HashMap<String, Course> = HashMap::new();
-    let course = Course::new("CS 383").await;
-    
-    println!("{:?}", course);
+    add_course_and_prerequisites("CS 380", &mut courses).await;
+    println!("{:?}", courses);
+}
+
+// I know there's a better way to do this
+fn add_course_and_prerequisites<'a> (
+    course_code: &'a str,
+    courses: &'a mut HashMap<String, Course>,
+) -> BoxFuture<'a, ()> {
+    async move {
+        let course = Course::new(course_code).await;
+        for chunk in &course.prerequisites {
+            for item in chunk {
+                add_course_and_prerequisites(item, courses).await;
+            }
+        }
+        courses.insert(course_code.to_string(), course);
+    }.boxed()
 }
